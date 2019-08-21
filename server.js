0b12ae9fa2b3553dacb0ce4d4b5ef6e94b9c3d04
Dealan <dealan.diongzon@gmail.com>
2019-08-21 16:00:39 -0400

    added boiler plate code for the server.js to intialize the app and then intialize the mongo database

---
+++ server.js
@@ -0,0 +1,30 @@
+// dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// scraping tools
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// require all models & set the port
+const db = require("./models");
+const PORT = 8000;
+
+// initialize express
+const app = express();
+
+// configure middleware
+
+// use morgan logger for logging requests
+app.use(logger("dev"));
+// parse request body as JSON
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+// make public a static folder
+app.use(express.static("public"));
+
+//Connect to the Mongo DB
+const MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
