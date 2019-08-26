818f0bf4cc20d77ab3e0ef430ae64181ef064ffb
Dealan <dealan.diongzon@gmail.com>
2019-08-25 22:22:26 -0400

    trying to figure out the post function but failing

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,15 +1,33 @@
-$(function() {
+$(function () {
 
-// this saves the user's preference of what page they want to get anime from
-$("#render").on("click", function(){
-    console.log(`this was clicked!`);
-    $.get("/scrape", function(){
-        console.log(`this was scraped!`)
-        location.reload();
+    // this saves the user's preference of what page they want to get anime from
+    $("#render").on("click", function () {
+        console.log(`this was clicked!`);
+        $.get("/scrape", function () {
+            console.log(`this was scraped!`)
+            location.reload();
+        })
     })
-})
 
-$(`.poop`).text("hello");
+    // this is the on click button for saving the note
+    $(document).on("click", "#save", function () {
+        console.log(`this was clicked!`);
+        let id = $(this).attr(`data-id`);
+        let comment = {
+            title: $(`.title`).val(),
+            body: $(`.body`).val(),
+        }
+        console.log(comment)
 
-console.log(`hello`);
+        $.ajax({
+            method: "POST",
+            url: "/anime/" + id,
+            data: comment
+          })
+            // With that done
+            .then(function(data) {
+              // Log the response
+              console.log(data);
+            });
+    })
 });
--- routes/routes.js
+++ routes/routes.js
@@ -23,7 +23,7 @@ module.exports = function (app) {
                 result.summary = $(this).find(`.anime-synopsis`).children(`p`).text();
                 result.link = $(this).find(`h3`).children(`a`).attr(`href`);
                 result.image = $(this).find(`.poster-container`).children(`img`).attr(`src`);
-                
+
                 // create a new article using the result object
                 db.Anime.create(result)
                     // .then(dbArticle => console.log(dbArticle))
@@ -35,16 +35,38 @@ module.exports = function (app) {
         });
     })
 
-    app.get("/", function(req,res){
+    app.get("/", function (req, res) {
         db.Anime.find({}).limit(4)
-        .then(function(dbArticle){
-            console.log(dbArticle);
-            var hbsObject = {
-                article: dbArticle
-            }
-            res.render("index", hbsObject)
+            .then(function (dbArticle) {
+                console.log(dbArticle);
+                var hbsObject = {
+                    article: dbArticle
+                }
+                res.render("index", hbsObject)
+            }).catch(function (err) {
+                res.json(err);
+            })
+    })
+
+    // route for grabbing a comment associated with the anime
+    app.get("/anime/:id", function(req,res){
+        db.Anime.findOne({_id: req.params.id}).populate(`comment`).then(function(dbAnime){
+            res.json(dbAnime);
         }).catch(function(err){
             res.json(err);
-        })
-      })
+        });
+    });
+
+    // route for saving a comment for the anime
+    app.post("/anime/:id", function (req, res) {
+        db.Comment.create(req.body)
+        .then(function (dbComment) {
+            return db.Anime.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
+        }).then(function (dbAnime) {
+            res.json(dbAnime);
+        }).catch(function (err) {
+            res.json(err);
+        });
+    });
+
 }
